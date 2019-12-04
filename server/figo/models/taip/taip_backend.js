6ffe876b397a3973d4f8cddd41515d71bd049270
rktel <rakinakuy@hotmail.com>
2019-12-04 09:25:10 -0500

    parser 2

--- server/figo/models/taip/taip_backend.js
+++ server/figo/models/taip/taip_backend.js
@@ -9,8 +9,9 @@ const ServerTCP = (serverPort, serverHost) => {
     server = createServer((socketIn) => {
         socketIn.on('data', (data) => {
             // console.log(data.toString())
-            PDU(data)
-           
+            const { mobileID } = PDU(data)
+            console.log(mobileID)
+
         })
     })
 
@@ -22,24 +23,25 @@ const ServerTCP = (serverPort, serverHost) => {
 
 
 const PDU = (raw) => {
-    // one raw
-    let mobileID = null
 
     const parser = (chunkraw) => {
         // console.log(chunkraw)
         chunkraw = chunkraw.split(";")
         // get ID
-        mobeliID = chunkraw[chunkraw.length - 1].match(/\d/g).join("")
+        return {
+            mobileID: chunkraw[chunkraw.length - 1].match(/\d/g).join("")
+        }
     }
     raw = raw.toString()
     // console.log(raw)
     raw = raw.split("\r\n")
-    // console.log(raw)
-    raw.map(element => {
-        element && parser(element)
-    })
 
-    console.log(mobileID)
+    const { mobileID } = parser(raw[0])
+
+    return {
+        mobileID
+    }
+
 }
 
 
