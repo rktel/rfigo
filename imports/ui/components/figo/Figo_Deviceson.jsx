c190823dbaf4af5ee8c7f9131c65cfc6f21faad7
rktel <rakinakuy@hotmail.com>
2019-12-05 09:56:52 -0500

    mobile containers 15

--- imports/ui/components/figo/Figo_Deviceson.jsx
+++ imports/ui/components/figo/Figo_Deviceson.jsx
@@ -40,7 +40,7 @@ const scriptList = [
 const Figo_Deviceson = (props) => {
     useEffect(() => {
         rstream.on('devices', (devices) => {
-            console.log(devices.length)
+            console.log(devices)
         })
     }, [])
     const ActionButtonGroup = () => (
