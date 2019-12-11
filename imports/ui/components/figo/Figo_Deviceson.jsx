fe6f31c1e11fb6d88e6ba601d2996e9b74cbb5b7
rktel <rakinakuy@hotmail.com>
2019-12-11 18:36:50 -0500

    reform DB 39

--- imports/ui/components/figo/Figo_Deviceson.jsx
+++ imports/ui/components/figo/Figo_Deviceson.jsx
@@ -50,7 +50,7 @@ const Figo_Deviceson = (props) => {
                 <Panel style={{ backgroundColor: 'blue', height: props.heightApp }}>
                     <InputGroup inside size="lg">
                         <Input />
-                        <InputGroup.Button>Enviar</InputGroup.Button>
+                        <InputGroup.Button><Button>Enviar</Button></InputGroup.Button>
                     </InputGroup>
                 </Panel>
             </FlexboxGrid.Item>
