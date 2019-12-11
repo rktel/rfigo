c3ef832e4de777e7d19ca886c914b9a19ecf245c
rktel <rakinakuy@hotmail.com>
2019-12-11 16:59:17 -0500

    reform DB 21

--- imports/ui/components/figo/Figo_Deviceson.jsx
+++ imports/ui/components/figo/Figo_Deviceson.jsx
@@ -24,14 +24,14 @@ const Figo_Deviceson = (props) => {
     const [selectedDevices, setSelectedDevices] = useState([])
     return (
 
-        <FlexboxGrid justify="start" align="top">
-            <FlexboxGrid.Item>
-                <Panel style={{ width: 300, height: props.heightApp, backgroundColor: 'gray' }}>
+        <FlexboxGrid>
+            <FlexboxGrid.Item colspan={8}>
+                <Panel style={{ backgroundColor: 'gray' }}>
                     Pepa
                 </Panel>
             </FlexboxGrid.Item>
-            <FlexboxGrid.Item>
-                <Panel style={{ height: props.heightApp, backgroundColor: 'blue' }}>
+            <FlexboxGrid.Item colspan={16}>
+                <Panel style={{ backgroundColor: 'blue' }}>
                     George
                 </Panel>
             </FlexboxGrid.Item>
