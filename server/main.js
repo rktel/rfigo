94038d6e678d23a3b8b77efa7c8453e4716c0229
rktel <rakinakuy@hotmail.com>
2020-01-21 13:17:54 -0500

    reset 2

--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 Meteor.startup(function () {
-    var restartFrequency = 1000 * 10; // 1 day (1000 millsec * 60 min * 24 hour)
+    var restartFrequency = 1000 * 60 * 60 ; // 1 Hora
     setTimeout(function () {
         process.exit();
     }, restartFrequency);
