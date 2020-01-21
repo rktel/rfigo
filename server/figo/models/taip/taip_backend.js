040344933bdbe5c75ba2264d01f5252b2a83bf81
rktel <rakinakuy@hotmail.com>
2020-01-21 15:22:41 -0500

    tcpserver uninstall

--- server/figo/models/taip/taip_backend.js
+++ server/figo/models/taip/taip_backend.js
@@ -178,23 +178,3 @@ const PDU = (raw) => {
 
 //DB_DevicesReset()
 //ServerTCP(7100, '0.0.0.0')
-
-const TcpServer = require('tcpserver');
- 
-const tcpServer = new TcpServer(7100, '0.0.0.0');
-tcpServer.start();
-tcpServer.callback['connect'] = function(client){
-    console.log('%s:%s connect.', client['socket'].remoteAddress, client['socket'].remotePort);
-    
-};
-tcpServer.callback['data'] = function(client, data){
-    console.log('%s:%s send: %s.', client['socket'].remoteAddress, client['socket'].remotePort, data.toString());
-    data = data.toString().trim();
-    //tcpServer.broadcast(client, data);
-    for (let x in tcpServer.clients) {
-        tcpServer.clients[x]['socket'].write(data);
-    }
-};
-tcpServer.callback['close'] = function(client){
-    console.log('%s:%s disconnect.', client['socket'].remoteAddress, client['socket'].remotePort);
-};
