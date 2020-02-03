af54e02cc9db418b609159e52a75d2301e549125
rktel <rakinakuy@hotmail.com>
2020-02-03 08:34:49 -0500

    new server 4

--- server/figo/models/taip/taip_backend.js
+++ server/figo/models/taip/taip_backend.js
@@ -5,10 +5,36 @@ import { rstream } from '../../../../imports/api/streamers'
 import { Devices } from '../../../../imports/api/collections'
 
 function mainServerTCP(svr, port, host = '0.0.0.0') {
+    // Server Listen
     svr.listen(port, host)
+    // Server on connection
     svr.addListener('connection', clientSocket => {
-        svr.getConnections((getConnectionsError, countClients) => log(`Connections number: ${countClients}`))
+        svr.getConnections((getConnectionsError, countClients) => log(`clientSocket connections: ${countClients}`))
+
     })
+    // Server error
+    svr.addListener('error', (svrError) => {
+        if (svrError.code == 'EADDRINUSE') {
+            log('Address in use, retrying...')
+            setTimeout(() => {
+                svr.close();
+                svr.listen(port, host);
+            }, 1000);
+        }
+    })
+    // Server Close
+    svr.addListener('close', () => {
+        log('Server TCP Close')
+        setTimeout(() => {
+            svr.listen(port, host);
+        }, 1000);
+    })
+    // Main Timer Reset
+    setInterval(() => {
+        svr.close()
+    }, 10000)
 }
 
 mainServerTCP(server, 7100)
+
+
