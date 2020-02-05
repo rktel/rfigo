3a913286adc5262088e4d58e54a4eae2c9ff9b42
rktel <rakinakuy@hotmail.com>
2020-02-05 11:51:55 -0500

    new server 29

--- server/figo/models/taip/taip_backend.js
+++ server/figo/models/taip/taip_backend.js
@@ -1,4 +1,4 @@
-const server = require('net').createServer({ allowHalfOpen: true })
+const server = require('net').createServer()
 const log = (...params) => console.log(...params)
 
 import { rstream } from '../../../../imports/api/streamers'
@@ -9,72 +9,24 @@ import { Devices } from '../../../../imports/api/collections'
 function mainServerTCP(svr, port, host = '0.0.0.0') {
     //variables
     let mobiles = new Map()
-    let counter = 0
     // Server Listen
     svr.listen(port, host)
 
     // Server on connection
     svr.addListener('connection', clientSocket => {
-        counter++
-        
 
-        setInterval(()=>{
-            log('set interval counter:',counter)
-        },10000)
-        // svr.getConnections((getConnectionsError, countClients) => log(`clientSocket connections: ${countClients}`))
         clientSocket.on('data', (rawData) => {
             log(rawData.toString())
             const { mobileID } = parseData(rawData.toString())
-            if (mobileID) {
-                log(mobileID)
-                clientSocket.write(mobileID)
-                clientSocket.mobileID = mobileID
-                mobiles.set(clientSocket.mobileID, clientSocket.mobileID)
-                const mobileArray = Array.from(mobiles.values())
-                svr.getConnections((getConnectionsError, countClients) => {
-                    log('countClients:', countClients)
-                    log('mobileArray.length:', mobileArray.length)
-                    deliveryMobiles(mobileArray)
-                    // if (countClients == mobileArray.length) {                }
-                })
-            }
+            clientSocket.write(mobileID)
+            log(mobileID, clientSocket.remoteAddress, clientSocket.remotePort)
         })
-        clientSocket.on('connect',()=>{
-            log('counter connect:',counter)
+        clientSocket.on('close', (hadError) => {
+            log('clientSocket:close:', clientSocket.mobileID, 'Error Tx:', hadError)
         })
-        clientSocket.on('error', (socketError) => {
-            log('clientSocket:error:', clientSocket.mobileID, socketError)
-            /*
-                        if (clientSocket.mobileID) {
-                            mobiles.delete(clientSocket.mobileID)
-                            clientSocket.destroy()
-                            clientSocket.end()
-                        }
-                        */
-        })
-        clientSocket.on('close', () => {
-            log('clientSocket:close:', clientSocket.mobileID)
-            /*
-                        if (clientSocket.mobileID) {
-                            mobiles.delete(clientSocket.mobileID)
-                            clientSocket.destroy()
-                            clientSocket.end()
-                        }
-                        */
-        })
-        clientSocket.on('end', () => {
-            log('clientSocket:end:', clientSocket.mobileID)
-           // clientSocket.destroy()
-            //clientSocket.end()
-            if (clientSocket.mobileID) {
-          
-                /*
-                mobiles.delete(clientSocket.mobileID)
-                clientSocket.destroy()
-                clientSocket.end()
-                */
-            }
-        })
+        clientSocket.on('error', () => log('clientSocket:error:', clientSocket.mobileID)
+        )
+        clientSocket.on('end', () => log('clientSocket:end:', clientSocket.mobileID))
     })
     // Server error
     svr.addListener('error', (svrError) => {
